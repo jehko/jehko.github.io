fd9cecc37a4e8d9ca7aed7ab96413cc501dd3f8a
jehko <krelude.jko@gmail.com>
2021-05-17 23:50:24 +0900

    contactme 레이아웃 완료. emailjs테스트 완료. send alert처리고민

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,53 +1,92 @@
-import React from "react";
-import { Jumbotron, Container, Row, Col, Form, Card, InputGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Jumbotron, Container, Row, Col, Form, Card, InputGroup, Toast } from "react-bootstrap";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  const SERVICE_ID = 'emailjs_jehko_gmail';
+  const TEMPLATE_ID = 'template_5zx2e8s';
+  const USER_ID = 'user_9L2TMeWCvoO0GCXpRtXVM';
+
+  const RESULT_MSG = ['메일 전송을 완료했습니다.', '메일 전송을 실패했습니다.'];
+  const TOAST_TIME = 2000;
+
+  const [inputName, setInputName] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputMsg, setInputMsg] = useState('');
+
+  const [resultMsg, setResultMsg] = useState();
+  const [showToast, setShowToast] = useState(false);
+
+  const resetInput = () => {
+    setInputName('');
+    setInputEmail('');
+    setInputMsg('');
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((result) => {
+        console.log(result);
+        setResultMsg(RESULT_MSG[0]);
+        resetInput();
+      })
+      .catch((error) => {
+        console.log(error);
+        setResultMsg(RESULT_MSG[1]);
+      })
+      .finally(() => {
+        setShowToast(true);
+      });
+  }
+
   return (
     <Jumbotron id="contact" className="Contact">
       <Container>
         <Row className="justify-content-center">
           <Col className="col-12 col-md-8 col-lg-6 pb-5">
-            <Form method="post">
+            <Form onSubmit={sendEmail}>
               <Card className="rounded-10">
                 <Card.Header className="p-0">
-                  <div class="bg-dark text-white text-center py-2">
-                    <h3>
-                      <i class="fa fa-envelope"></i> Contact Me
-                    </h3>
-                    <p class="m-0"></p>
+                  <div className="bg-dark text-white text-center py-2">
+                    <h3><i className="fa fa-envelope"></i> Contact Me</h3>
+                    <p className="m-0"></p>
                   </div>
                 </Card.Header>
                 <Card.Body className="p-3">
                   <Form.Group>
                     <InputGroup className="mb-2">
-                      <div class="input-group-prepend">
-                        <div class="input-group-text">
-                          <i class="fa fa-user text-info"></i>
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">
+                          <i className="fa fa-user text-info"></i>
                         </div>
                       </div>
                       <input
                         type="text"
-                        class="form-control"
-                        id="nombre"
-                        name="nombre"
+                        className="form-control"
+                        name="name"
                         placeholder="이름"
+                        value={inputName}
+                        onChange={(e) => setInputName(e.target.value)}
                         required
                       />
                     </InputGroup>
                   </Form.Group>
                   <Form.Group>
                     <InputGroup className="mb-2">
-                      <div class="input-group-prepend">
-                        <div class="input-group-text">
-                          <i class="fa fa-envelope text-info"></i>
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">
+                          <i className="fa fa-envelope text-info"></i>
                         </div>
                       </div>
                       <input
                         type="email"
-                        class="form-control"
-                        id="nombre"
+                        className="form-control"
                         name="email"
                         placeholder="보내시는 분"
+                        value={inputEmail}
+                        onChange={(e) => setInputEmail(e.target.value)}
                         required
                       />
                     </InputGroup>
@@ -55,26 +94,37 @@ function Contact() {
 
                   <Form.Group>
                     <InputGroup className="mb-2">
-                      <div class="input-group-prepend">
-                        <div class="input-group-text">
-                          <i class="fa fa-comment text-info"></i>
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">
+                          <i className="fa fa-comment text-info"></i>
                         </div>
                       </div>
                       <textarea
-                        class="form-control"
+                        className="form-control"
+                        rows="4"
+                        name="message"
                         placeholder="메시지"
+                        value={inputMsg}
+                        onChange={(e) => setInputMsg(e.target.value)}
                         required
                       ></textarea>
                     </InputGroup>
                   </Form.Group>
 
-                  <div class="text-center">
+                  <div className="text-center">
                     <input
                       type="submit"
                       value="SEND"
-                      class="btn btn-dark rounded-8 py-2 w-10"
+                      className="btn btn-dark rounded-8 py-2 w-10"
                     />
                   </div>
+                  {showToast &&
+                    <div style={{position:'absolute',top:0, bottom:0, left:0, right:0, display:'flex', justifyContent:'center', alignItems:'center'}}>
+                      <Toast onClose={() => {setShowToast(false)}} show={showToast} delay={TOAST_TIME} autohide>
+                        <Toast.Header><strong className="mr-auto">{ resultMsg }</strong></Toast.Header>
+                      </Toast>
+                    </div>
+                  }
                 </Card.Body>
               </Card>
             </Form>
